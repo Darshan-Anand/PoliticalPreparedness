56536e557c15e738c08c50187cd42359bac04920
Darshan Anand <badarshan1998@gmail.com>
2022-01-24 09:41:51 +0530

    Removed TODO commits

--- app/src/main/java/com/example/android/politicalpreparedness/network/CivicsApiService.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/network/CivicsApiService.kt
@@ -15,7 +15,6 @@ import retrofit2.http.Query
 
 private const val BASE_URL = "https://www.googleapis.com/civicinfo/v2/"
 
-// TODO: Add adapters for Java Date and custom adapter ElectionAdapter (included in project)
 private val moshi = Moshi.Builder()
     .add(ElectionAdapter())
     .add(DateAdapter())
--- app/src/main/java/com/example/android/politicalpreparedness/representative/adapter/RepresentativeListAdapter.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/representative/adapter/RepresentativeListAdapter.kt
@@ -104,4 +104,3 @@ class RepresentativeDiffCallback : DiffUtil.ItemCallback<Representative>() {
     }
 }
 
-//TODO: Create RepresentativeListener*/
